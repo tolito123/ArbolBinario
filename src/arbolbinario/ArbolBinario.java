59935128e93600ee8faa10e37759fa37833e6081
tolito123 <cjoseantonio447@gmail.com>
2024-10-24 00:07:20 -0600

    Se agrega el proyecto

---
+++ src/arbolbinario/ArbolBinario.java
@@ -0,0 +1,54 @@
+package arbolbinario;
+
+public class ArbolBinario {
+
+    public static void main(String[] args) {
+        //Nodo raiz = new Nodo();
+        //raiz.valor=60;
+        
+        /*
+        Nodo raiz = new Nodo();
+        raiz.valor=60;
+        
+        raiz.NodoIzq = new Nodo();
+        raiz.NodoIzq.valor=41;
+        
+        raiz.NodoIzq = new Nodo();
+        raiz.NodoIzq.valor=16;
+        
+        raiz.NodoIzq = new Nodo();
+        raiz.NodoIzq.valor=25;
+        
+        raiz.NodoDer = new Nodo();
+        raiz.NodoDer.valor=53;
+        
+        raiz.NodoIzq = new Nodo();
+        raiz.NodoIzq.valor=46;
+        
+        raiz.NodoIzq = new Nodo();
+        raiz.NodoIzq.valor=42;
+        
+        raiz.NodoDer = new Nodo();
+        raiz.NodoDer.valor=55;
+        
+        raiz.NodoDer = new Nodo();
+        raiz.NodoDer.valor=74;
+        
+        raiz.NodoIzq = new Nodo();
+        raiz.NodoIzq.valor=65;
+        
+        raiz.NodoIzq = new Nodo();
+        raiz.NodoIzq.valor=63;
+        
+        raiz.NodoIzq = new Nodo();
+        raiz.NodoIzq.valor=62;
+        
+        raiz.NodoDer = new Nodo();
+        raiz.NodoDer.valor=64;
+        
+        raiz.NodoDer = new Nodo();
+        raiz.NodoDer.valor=70;
+        */
+    }
+    
+}
---
+++ src/arbolbinario/Nodo.java
@@ -0,0 +1,27 @@
+package arbolbinario;
+
+
+
+public class Nodo {
+    /*int valor;
+    int derecho;
+    int Izquierdo;
+    public void setvalor(int valor){
+        this.valor=valor;
+        
+    }
+    
+    public void setNodoDer(int derecho) { 
+        this.derecho=derecho;
+        
+    }
+    public void setIzquierdo(int Izquierdo) { 
+        this.Izquierdo=Izquierdo;
+        
+    }*/
+    /*
+    public int valor {get; set;}
+    public Nodo NodoIzq{get; set;}
+    public Nodo NodoDer{get; set;}
+    */
+}
